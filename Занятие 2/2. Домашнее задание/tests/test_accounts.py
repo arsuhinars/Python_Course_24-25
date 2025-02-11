859d02790582c316f5d2f295de03b05f8e158d99
Arseny <arsuhinars@gmail.com>
2025-02-11 09:33:08 +0400

    Added 7th class

---
+++ Занятие 2/2. Домашнее задание/tests/test_accounts.py
@@ -0,0 +1,18 @@
+class TestAccountsRegister:
+    async def test_valid(self):
+        pass
+
+    async def test_empty_login(self):
+        pass
+
+    async def test_empty_password(self):
+        pass
+
+    async def test_short_password(self):
+        pass
+
+    async def test_empty_name(self):
+        pass
+
+    async def test_invalid(self):
+        pass
